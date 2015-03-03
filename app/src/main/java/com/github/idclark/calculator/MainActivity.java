314d4672e7ba88fa86716219180ff5e597a92240
idclark <idclark13@gmail.com>
2015-03-03 18:31:36 -0500

    initial working calculator view

--- app/src/main/java/com/github/idclark/calculator/MainActivity.java
+++ app/src/main/java/com/github/idclark/calculator/MainActivity.java
@@ -9,9 +9,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +59,9 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class CalculatorFragment extends Fragment {
+    public static class CalculatorFragment extends Fragment implements OnClickListener {
+
+        private TextView mCalculatorDisplay;
 
         public CalculatorFragment() {
         }
@@ -64,7 +70,37 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            mCalculatorDisplay = (TextView) rootView.findViewById(R.id.textView);
+
+            rootView.findViewById(R.id.AC).setOnClickListener(this);
+            rootView.findViewById(R.id.plusminus).setOnClickListener(this);
+            rootView.findViewById(R.id.percent).setOnClickListener(this);
+            rootView.findViewById(R.id.nine).setOnClickListener(this);
+            rootView.findViewById(R.id.eight).setOnClickListener(this);
+            rootView.findViewById(R.id.seven).setOnClickListener(this);
+            rootView.findViewById(R.id.six).setOnClickListener(this);
+            rootView.findViewById(R.id.five).setOnClickListener(this);
+            rootView.findViewById(R.id.four).setOnClickListener(this);
+            rootView.findViewById(R.id.three).setOnClickListener(this);
+            rootView.findViewById(R.id.two).setOnClickListener(this);
+            rootView.findViewById(R.id.one).setOnClickListener(this);
+            rootView.findViewById(R.id.zero).setOnClickListener(this);
+            rootView.findViewById(R.id.div).setOnClickListener(this);
+            rootView.findViewById(R.id.mult).setOnClickListener(this);
+            rootView.findViewById(R.id.plus).setOnClickListener(this);
+            rootView.findViewById(R.id.minus).setOnClickListener(this);
+            rootView.findViewById(R.id.dec).setOnClickListener(this);
+            rootView.findViewById(R.id.equal).setOnClickListener(this);
+
             return rootView;
+        }
+
+        @Override
+        public void onClick(View v) {
+            String buttonPressed = ((Button )v).getText().toString();
+            mCalculatorDisplay.setText(buttonPressed);
+
         }
     }
 }
